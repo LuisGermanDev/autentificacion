e93be4f78756c1a089dbc1313049d7989af2e02f
luisgerman <27436197+LuisGermanDev@users.noreply.github.com>
2024-10-09 13:52:34 -0400

    agregar ruta login

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 //mongoDB
 
 mongoose
-  .connect("")
+  .connect("mongodb://localhost:27017/Usuario")
   .then(() => console.log("Conectado a MongoDB"))
   .catch((error) => console.log("Error al conectar en mongoDB", error));
 
@@ -34,10 +34,23 @@ app.post("/regiter", async (req, res) => {
       email,
       password: hashedPassword,
       dirreciones,
-    })
+    });
     await nuevoUsuario.save();
     res.status(201).send("usuario registrado");
   } catch (error) {
     res.status(400).send("error en el registro" + error.message);
   }
 });
+app.post('./login',async(req,res)=>{
+    const {email,password}=req.body;
+    try {
+        const usuario = await Usuario.findOne({email});
+        if (!usuario) return res.status(400).send('Usuario no encontrado');
+        const passwordCorrecta = await bcrypt.compare(password,usuario.password);
+        if(!passwordCorrecta) return res.status(400).send('contrasena incorrecta');//no es recomendable avisar si la contra o elusuario es incorrecto, solo con fine academicos
+        const token = jwt.sign({id:usuario._id},SECRET_KEY,{expireIn:'1h'});
+        re.status(200).json({token})// estamos cacturando el token aqui pero en otros casos lo deberiamos guardar enotro lugar seguro
+    } catch (error) {
+        res.status(500).send('Error al inicio de sesion'+error.message);
+    }
+})
